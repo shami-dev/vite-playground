d7d6e5c673eb7fc70db7616b275349c68dc48b17
Shamil Khaibullov <shamil@shamil.dev>
2025-07-06 18:24:40 +0400

    chore: prepare Node.js project setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,14 @@
 import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react'
-import jsxA11y from 'eslint-plugin-jsx-a11y';
 import prettier from 'eslint-config-prettier'
+import globals from 'globals'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'vite.config.js', 'node_modules/']),
+  globalIgnores(['dist', 'node_modules/']),
   {
-    files: ['**/*.{js,jsx}'],
-    plugins: {
-      react,
-      'jsx-a11y': jsxA11y,
-    },
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-      prettier
-    ],
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
+    extends: [js.configs.recommended, prettier],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.node,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -37,8 +17,6 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react/jsx-uses-react': 'error',
-      'react/jsx-uses-vars': 'error',
     },
   },
 ])
